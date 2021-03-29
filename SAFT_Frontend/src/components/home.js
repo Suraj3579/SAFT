16b109938e380ad2023233ee3618c648e751d40b
dhanush1708 <dhanush@iitg.ac.in>
2021-03-29 17:01:32 +0530

    added signup form on home page

--- SAFT_Frontend/src/components/home.js
+++ SAFT_Frontend/src/components/home.js
@@ -2,27 +2,147 @@ import React from "react";
 import ButtonAppBar from "./ButtonAppBar";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, CssBaseline, Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 const useStyles = makeStyles((theme) => ({
+  imageForm: {
+    display: "flex",
+  },
+
   image: {
     backgroundImage:
       "url(https://www.dealsshutter.com/blog/wp-content/uploads/2020/03/homeservice.jpg)",
-    width: "100vw",
+    width: "66%",
     height: "90vh",
     backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    marginTop: theme.spacing(10),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
   },
 }));
 
 const Home = () => {
   const classes = useStyles();
   return (
-    <div>
+    <Grid>
       <CssBaseline />
       <ButtonAppBar lab="h" />
-      <div className={classes.image}>
-        <Typography variant="h6">.</Typography>
-      </div>
-    </div>
+      <Grid className={classes.imageForm}>
+        <Grid className={classes.image}></Grid>
+        <Grid>
+          <Container component="main" maxWidth="xs">
+            <Grid className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign up
+              </Typography>
+              <form className={classes.form} noValidate>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      autoComplete="fname"
+                      name="firstName"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="lname"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox value="allowExtraEmails" color="primary" />
+                      }
+                      label="I want to receive inspiration, marketing promotions and updates via email."
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Sign Up
+                </Button>
+                <Grid container justify="flex-end">
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      Already have an account? Sign in
+                    </Link>
+                  </Grid>
+                </Grid>
+              </form>
+            </Grid>
+            {/* <Box mt={5}>
+        <Copyright />
+      </Box> */}
+          </Container>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
