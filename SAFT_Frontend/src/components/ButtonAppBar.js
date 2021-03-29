6f725f8d64defe3a325381420bd7d8c11b9112d6
dhanush1708 <dhanush@iitg.ac.in>
2021-03-29 14:27:32 +0530

    added logo

--- SAFT_Frontend/src/components/ButtonAppBar.js
+++ SAFT_Frontend/src/components/ButtonAppBar.js
@@ -7,44 +7,53 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "@material-ui/core";
-import { createMuiTheme } from "@material-ui/core/styles";
+import logo from "../images/logo.jpeg";
+// import { createMuiTheme } from "@material-ui/core/styles";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "white",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000",
-    },
-  },
-});
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       light: "white",
+//       main: "#3f50b5",
+//       dark: "#002884",
+//       contrastText: "#fff",
+//     },
+//     secondary: {
+//       light: "#ff7961",
+//       main: "#f44336",
+//       dark: "#ba000d",
+//       contrastText: "#000",
+//     },
+//   },
+// });
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    flex: 1,
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
-    color: theme.palette.primary.dark,
+    flex: 10,
+  },
+  button: {
+    flex: 1,
+    color: "orange",
+    sizeLarge: "1",
+  },
+  logo: {
+    height: "10vh",
+    weight: "10vw",
   },
 }));
 
 export default function ButtonAppBar() {
-  const classes = useStyles(theme);
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar color="transparent">
+      <AppBar color="white" style={{ position: "relative" }}>
         <Toolbar>
           <IconButton
             edge="start"
@@ -54,16 +63,11 @@ export default function ButtonAppBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            {/* <a href="/">SAFT</a> */}
-            <Link href="/" underline="none">
-              SAFT
-            </Link>
-          </Typography>
-          <Button color={theme.palette.primary.dark} href="/login">
+          <img src={logo} className={classes.logo} />
+          <Button size="large" href="/login" className={classes.button}>
             Login
           </Button>
-          <Button color="secondary" href="/signUp">
+          <Button size="large" href="/signUp" className={classes.button}>
             SignUp
           </Button>
         </Toolbar>
