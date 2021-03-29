f2e56990c89de4ef8d1ea9c99d9b4a0845300330
dhanush1708 <dhanush@iitg.ac.in>
2021-03-29 13:24:40 +0530

    removed margin

--- SAFT_Frontend/src/components/home.js
+++ SAFT_Frontend/src/components/home.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ButtonAppBar from "./ButtonAppBar";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { Box, CssBaseline, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   image: {
     backgroundImage:
       "url(https://mobisoftinfotech.com/resources/wp-content/uploads/2018/08/Banner.png)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
+    // backgroundRepeat: "no-repeat",
+    // backgroundColor:
+    //   theme.palette.type === "light"
+    //     ? theme.palette.grey[50]
+    //     : theme.palette.grey[900],
+    // backgroundSize: "cover",
+    // backgroundPosition: "center",
   },
 }));
 
@@ -21,9 +21,10 @@ const Home = () => {
   const classes = useStyles();
   return (
     <div>
+      <CssBaseline />
       <ButtonAppBar />
       <div className={classes.image}>
-        <Typography variant="h1">Hello</Typography>
+        <Typography variant="h6">.</Typography>
       </div>
     </div>
   );
